491ca37c39fe61184dce7d278ed076551e842669
Dave Briccetti <daveb@davebsoft.com>
2022-08-23 12:31:44 -0700

    Add Sunday School and Dorm Room

--- young_sheldon_game.py
+++ young_sheldon_game.py
@@ -19,33 +19,53 @@ class YoungSheldon(Game):
         home.add_events(relaxation_event, feynman_poster_event)
 
         # Billy Sparks’s house
-        sparkses = Place('Billy Sparks’s', 'Billy Sparks’s House')
+        billys = Place('Billy Sparks’s', 'Billy Sparks’s House')
         chicken_event = Event(0.3, 'Billy’s chicken scares you', -10)
         chicken_event.add_else_events(Event(1, 'Billy says something kind', 5))
-        sparkses.add_events(chicken_event)
+        billys.add_events(chicken_event)
 
         # Meemaw’s
         meemaws = Place('Meemaw’s', 'Meemaw’s house')
-        meemaws.add_events(Event(0.4, 'Meemaw makes cookies', 20))
-        meemaws.add_events(Event(0.3, 'We play video games', 20))
+        meemaws.add_events(
+            Event(0.4, 'Meemaw makes cookies', 20),
+            Event(0.3, 'We play video games', 20),
+            Event(0.05, 'Meemaw is mad at you', -10),
+        )
+
+        # Sunday School
+        sunday_school = Place('Sunday School', 'Sunday School')
+        sunday_school.add_events(
+            Event(0.2, 'Paige makes you mad', -20),
+            Event(0.3, 'You win an argument with Pastor Jeff about the existence of God', 20),
+            Event(0.1, 'Billy says something funny', 5),
+        )
 
         # University
         university = Place('University', 'Your university')
+        university.add_events(
+            Event(0.1, 'Your mother embarrasses you', -5),
+            Event(0.6, 'You are glad to be at your place of higher learning', 5),
+        )
+
+        # Dorm Room
+        dorm_room = Place('Dorm Room', 'Your Dorm Room')
 
         # Dr. Sturgis’s class
         sturgis_class = Place('Dr. Sturgis’s class', 'Your front row seat in Dr. Sturgis’s class')
 
         # Dr. Linkletter’s office
-        linkletter_office = Place('Linkletter’s office', 'Dr. Linkletter’s office')
-        linkletter_office.add_events(Event(1, 'Dr. Linkletter is not happy to see you', -10))
+        linkletters_office = Place('Linkletter’s office', 'Dr. Linkletter’s office')
+        linkletters_office.add_events(Event(1, 'Dr. Linkletter is not happy to see you', -10))
 
         # Transitions
-        home.add_transitions(meemaws, university, sparkses)
+        home.add_transitions(meemaws, university, billys, sunday_school)
         meemaws.add_transitions(home)
-        university.add_transitions(home, sturgis_class, linkletter_office)
+        university.add_transitions(home, dorm_room, sturgis_class, linkletters_office)
         sturgis_class.add_transitions(university)
-        linkletter_office.add_transitions(university)
-        sparkses.add_transitions(home)
+        linkletters_office.add_transitions(university)
+        dorm_room.add_transitions(university)
+        billys.add_transitions(home)
+        sunday_school.add_transitions(home)
 
         # Starting place
         self.location = home
